b26ef41d8766842969250d4e28a64a139fb05f16
kristeehan <kristeehan@gmail.com>
2025-04-24 07:05:07 -0400

    demo instructions and adjustments

--- demo.js
+++ demo.js
@@ -3,9 +3,16 @@ import Highlighter from "./highlighter.js";
 const highlighter = new Highlighter();
 const $trs = document.querySelectorAll('tr');
 const $demoButton = document.getElementById('demo-button');
+const $p = document.querySelector('p');
+const $link = document.getElementById('link');
 
 $demoButton.addEventListener('click', (e) => {
     e.preventDefault();
     const randomIndex = Math.floor(Math.random() * $trs.length);
     highlighter.highlight($trs[randomIndex]);
+})
+
+$link.addEventListener('click', (e) => {
+    e.preventDefault();
+    highlighter.highlight($p);
 })
